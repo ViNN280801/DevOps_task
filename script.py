0e4959f9f64f228451f9b28926a9a9c40f7ac9eb
Vladislav <vladislav_semykin01@mail.ru>
2023-01-12 10:50:10 +0300

    Improvements in according to rules of formatting: https://peps.python.org/pep-0008/

--- script.py
+++ script.py
@@ -1,90 +1,122 @@
-# Importing libraries to work with system, time, difference of some objects
-from re import search
-import sys, datetime, difflib
+import os
+import sys
+import datetime
+import difflib
 
-# Importing colors library
+from re import search
 from colorama import init
 from termcolor import colored
 
 # Using colorama to make termcolor work on Windows
 init()
 
-# Importing regex library
-import re
-
-# Importing 'search' from regex library
 # Constant variable initialization
 # Where 'datetime.datetime.now().timestamp()' -> is time stamp
 # and 'datetime.datetime.fromtimestamp()' -> converts time stamp to
 # normal date and time, for example: 2023-01-11 18:40:31.988340
 TIME_STAMP = datetime.datetime.fromtimestamp(
     datetime.datetime.now().timestamp())
-print(f"Time stamp: {TIME_STAMP}")
 
-# Defining function that will be show a menu
+
+def clearConsole():
+    ''' Clearing up console output '''
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+def isOnlyDigits(string):
+    ''' Returns "True" if 'string' is contains only digits
+    "False" - otherwise '''
+    if search("\d+", string):
+        return True
+    else:
+        return False
+
+
 def menu():
-    print('1. View file\n2. Compare files\n3. Exit\nEnter a value: ', end='')
-    return int(input())
+    ''' Prints menu and current date and time to terminal. Returns input value '''
+    print(f"Time stamp: {TIME_STAMP}")
+    print('1. View file\n2. Compare files\n3. Clear output\n4. Exit\nEnter a value: ', end='')
+    return input()
 
-# Defining function that will print contents of a file
+
 def printContentsOfFile(filename):
+    ''' Just prints file content to terminal, with "end=''" '''
     with open(filename, 'r') as f:
         for line in f:
             print(line, end='')
 
-# Returns found line which is matches with pattern
-def findStringInFile(filename, pattern="^compiler=\w"):
+
+def findStringInFile(filename, pattern='^compiler=\w'):
+    ''' Returns line which is respond to specified pattern '''
     with open(filename, 'r') as f:
         for line in f.readlines():
-            if re.search(pattern, line):
+            if search(pattern, line):
                 return line
 
-# Emulating 'diff' command
-def compareFiles(filenames):
-    with open(filenames[0], 'r') as f1:
-        with open(filenames[1], 'r') as f2:
-            if findStringInFile(filenames[0]) == findStringInFile(filenames[1]):
-                print(colored("The files differ only in the time and place of assembly", 'blue'))
-            else:
-                diff = difflib.unified_diff(f1.readlines(), f2.readlines())
-                for line in diff:
-                    if line[0] == '+':
-                        print(colored(line, 'green'), end='')
-                    elif line[0] == '-':
-                        print(colored(line, 'red'), end='')
-                    else:
-                        print(line, end='')
 
-# Defining function of looped menu
+def compareFiles(filenames):
+    ''' Comparing 2 files like 'diff' linux command. Prints
+    results to the terminal, inserted marked with green, deleted - with red,
+    neutral lines marked with white '''
+    with open(filenames[0], 'r') as f1, \
+            open(filenames[1], 'r') as f2:
+        if findStringInFile(filenames[0]) == findStringInFile(filenames[1]):
+            print(
+                colored("The files differ only in the time and place of assembly", 'blue'))
+        else:
+            diff = difflib.unified_diff(f1.readlines(), f2.readlines())
+            for line in diff:
+                if line[0] == '+':
+                    print(colored(line, 'green'), end='')
+                elif line[0] == '-':
+                    print(colored(line, 'red'), end='')
+                else:
+                    print(line, end='')
+
+
 def loopedMenu(filenames):
+    ''' Function same as 'menu()', but looped and processing users input '''
     while True:
         choice = menu()
-        if choice == 1:
-            while True:
-                print('Which of file do you want to view?\n1. {0}\n2. {1}\nEnter a number: '.format(
-                    filenames[0], filenames[1]), end='')
-                choice = input()
-                if int(choice) == 1:
-                    printContentsOfFile(filenames[0])
-                    break
-                elif int(choice) == 2:
-                    printContentsOfFile(filenames[1])
-                    break
-                else:
-                    print('Incorrect input')
-
-        elif choice == 2:
-            compareFiles(filenames)
-        elif choice == 3:
-            print('Exiting ...')
-            break
+        if isOnlyDigits(choice):
+            if int(choice) == 1:
+                while True:
+                    print('Which of file do you want to view?\n1. {0}\n2. {1}\nEnter a number: '.format(
+                        filenames[0], filenames[1]), end='')
+                    choice = input()
+                    if isOnlyDigits(choice):
+                        if int(choice) == 1:
+                            printContentsOfFile(filenames[0])
+                            break
+                        elif int(choice) == 2:
+                            printContentsOfFile(filenames[1])
+                            break
+                        else:
+                            print(colored('Error! Incorrect input', 'red'))
+                    else:
+                        print(
+                            colored('Your input is invalid! You entered not only digits', 'red'))
+            elif int(choice) == 2:
+                compareFiles(filenames)
+            elif int(choice) == 3:
+                clearConsole()
+            elif int(choice) == 4:
+                print('Exiting ...')
+                break
+            else:
+                print(colored('Error! Incorrect input', 'red'))
         else:
-            print('Incorrect input')
+            print(colored('Your input is invalid! You entered not only digits', 'red'))
+
 
 def main():
     # Getting names of files which are been passed to CLI
-    # by command "py script.py <file1> <file2>"
+    # by command "python3 script.py <file1> <file2>"
     filenames = sys.argv[1:]
     loopedMenu(filenames)
+
 
 main()
